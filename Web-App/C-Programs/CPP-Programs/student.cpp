49060b87b54c4daa9161bf9a3abab218b41397d8
aakankshalakkas24-ctrl <aakankshalakkas24@gmail.com>
2025-12-28 14:34:30 +0530

    Create student.cpp

---
+++ Web-App/C-Programs/CPP-Programs/student.cpp
@@ -0,0 +1,30 @@
+CPP-Programs/student.cpp
+  #include <iostream>
+using namespace std;
+
+class Student {
+    int marks;
+public:
+    void input() {
+        cout << "Enter marks: ";
+        cin >> marks;
+    }
+
+    void displayGrade() {
+        if (marks >= 90)
+            cout << "Grade: A";
+        else if (marks >= 75)
+            cout << "Grade: B";
+        else if (marks >= 50)
+            cout << "Grade: C";
+        else
+            cout << "Grade: Fail";
+    }
+};
+
+int main() {
+    Student s;
+    s.input();
+    s.displayGrade();
+    return 0;
+}
